1087e11139cbbf9f7c78a4950f3f5ee34a8eeb63
Austin Mayer <austinmayer9@gmail.com>
2021-06-05 17:16:02 -0700

    Init Vue Projects with Vite, Tailwind, ESLinter, and Prettier formatting.

---
+++ vite-app/src/components/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="font-bold text-2xl">
+      About
+  </div>
+</template>
+
+<script setup>
+
+</script>
---
+++ vite-app/src/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  
+<h1 class="mb-6 text-3xl font-extrabold">Home</h1>
+
+</template>
+
+<script setup>
+
+</script>

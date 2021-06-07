3632fec25a511944c8d468987768039e754d4398
Austin Mayer <austinmayer9@gmail.com>
2021-06-07 11:49:31 -0700

    Added Google Font, created basic html template in Home.vue, added a gradient background via Tailwind.

--- vite-app/src/components/Home.vue
+++ vite-app/src/components/Home.vue
@@ -1,9 +1,23 @@
 <template>
-  
-<h1 class="mb-6 text-3xl font-extrabold">Home</h1>
+    <head>
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap"
+    rel="stylesheet"
+  />
+  </head>
 
+    <body >
+        <div class="h-screen bg-gradient-to-b from-gray-500 to-gray-900">
+            <nav>
+                <div class="font-cairo text-white text-center mb-6 text-3xl">
+                    <h1 class="mr-10">Austin</h1>
+                    <h1 class="ml-10">Mayer</h1>
+                </div>
+            </nav>
+        </div>
+    </body>
 </template>
 
 <script setup>
-
 </script>

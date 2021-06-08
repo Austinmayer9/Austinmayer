56b0507cc3aeeb95e7e5f93086d1818b3c1fc184
Austin Mayer <austinmayer9@gmail.com>
2021-06-08 11:49:15 -0700

    Added paint.jpg, made paint.jpg background image, added to tailwind.config.js.

--- vite-app/tailwind.config.js
+++ vite-app/tailwind.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        cairo: ['Cairo', 'sans-serif']
-    }
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            backgroundImage: (theme) => ({
+                'paint': "url('/src/assets/paint.jpg')",
+            }),
+            fontFamily: {
+                cairo: ['Cairo', 'sans-serif'],
+            },
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
